daa42efc06c11481db6bde5c0412285dc9290a2b
KhanhDw <giakhanh2207dev@gmail.com>
2025-08-06 23:04:34 +0700

    upgrate url logo header

--- src/components/layout/partials/header.jsx
+++ src/components/layout/partials/header.jsx
@@ -166,7 +166,7 @@ function Sidebar({ isSidebarOpen, setIsSidebarOpen }) {
         <div className='flex items-start justify-start flex-col w-full'>
           <div className='flex items-center justify-between mb-4'>
             <div className='flex items-center justify-start xs:h-10 2xl:h-20 px-3 py-2 overflow-hidden rounded-2xl w-fit'>
-              <img src="./logoModi.png" className='xs:h-5 sm:h-10 md:h-7 lg:h-7 xl:h-8 2xl:h-12 3xl:h-15 w-fit' alt='logo' />
+              <img src="/logoModi.png" className='xs:h-5 sm:h-10 md:h-7 lg:h-7 xl:h-8 2xl:h-12 3xl:h-15 w-fit' alt='logo' />
             </div>
             <button onClick={() => setIsSidebarOpen(false)} type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
               <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
