de4b98c3d0d4e55121db84e490bc8a424912851a
TranVanDuy1003 <tranvanduy31012016@gmail.com>
2025-10-05 03:23:46 +0700

    responsive modal, pagination contact

--- src/components/admin/common/Modal.jsx
+++ src/components/admin/common/Modal.jsx
@@ -3,7 +3,7 @@ export default function Modal({ isOpen, onClose, title, children }) {
 
   return (
     <div className="fixed inset-0 bg-black/70 bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white admin-dark:bg-gray-800 p-6 rounded-lg max-w-2xl w-full mx-4 max-h-[80vh] overflow-y-auto">
+      <div className="bg-white admin-dark:bg-gray-800 px-3 py-4 rounded-lg max-w-2xl w-full mx-2 sm:mx-4 max-h-[80vh] overflow-y-auto">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-lg font-semibold text-black admin-dark:text-gray-100">{title}</h2>
           <button onClick={onClose} className="text-gray-500 hover:text-gray-700 cursor-pointer">
